aedd8bf03ddee36793ba5ee1eb24d3e57036cd5e
Damilola <mattoyewumi440@gmail.com>
2023-03-17 07:44:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line using a printf function
+ * Return: always 0 for success execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
