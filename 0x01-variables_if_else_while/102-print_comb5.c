24aa33c9ce1e71cc86c32da16cff5498c63c2be4
Damilola <mattoyewumi440@gmail.com>
2023-03-18 16:27:20 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function of a program that prints numbers
+ * Return: be 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 98 ; i++)
+	{
+		for (j = 1 ; j <= 99 ; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
