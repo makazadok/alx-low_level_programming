f4a71bd159664231c35cf3ae42637d66c9665b75
Damilola <mattoyewumi440@gmail.com>
2023-03-17 20:57:21 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function to print numbers
+ * Return: be 0 for successful execution
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+		return (0);
+}
