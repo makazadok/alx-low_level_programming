3858594f9c0cc0f093331cd27db36c382b1c2eed
Damilola <mattoyewumi440@gmail.com>
2023-03-18 06:04:45 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function to print comb of numbers
+ * Return: be 0 for successful execution
+ */
+int main(void)
+{       
+        int i;
+	for (i = 0 ; i < 10 ; i++)
+		(
+		 putchar(i);
+		 putchar(',');
+		 putchar(' ');
+		 )
+			putchar('\n')
+			return (0)
+}
