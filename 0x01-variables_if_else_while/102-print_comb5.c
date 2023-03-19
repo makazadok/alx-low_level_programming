1b0c82600e4d6d35c60cb315afd472c08c522d63
Damilola <mattoyewumi440@gmail.com>
2023-03-19 12:53:15 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,13 +19,10 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if (i == 98 && j == 99)
+			if (i <= 98 && j <= 99)
 			{
-				break;
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
