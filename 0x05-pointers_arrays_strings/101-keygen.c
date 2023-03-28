0c6ee6d196bd85babcadca00e1b633b73386c034
Damilola <mattoyewumi440@gmail.com>
2023-03-28 09:19:05 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	srand(time(NULL));
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int index = rand() % sizeof(charset);
+		password[i] = charset[index];
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
